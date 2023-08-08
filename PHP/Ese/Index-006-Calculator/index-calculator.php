65e8379bfd5bf772a7a7132bb2eb2f955dc24adb
Martina Spagnolo <mar.spagnolo@gmail.com>
2023-08-08 18:52:21 +0200

    correzione esercizio calcolatore

--- PHP/Ese/Index-006-Calculator/index-calculator.php
+++ PHP/Ese/Index-006-Calculator/index-calculator.php
@@ -37,38 +37,39 @@ Il vostro compito sarà quello di creare 3 metodi (pubblici, protetti, statici v
 Tutte le operazioni che andrete a fare dovranno essere richiamate dal trait sopra definito.
 */
 
-class Rettagolo {
+class Rettagolo
+{
 
-    use Calculator;
-    public $b;
-    public $a;
+  use Calculator;
 
-    public function __construct($b, $a) {
+  public $altezza;
+  public $base;
 
-        $this->b=$b;
-        $this->a=$a;
-    }
+  public function __construct($altezza, $base)
+  {
 
-    public function getArea ($b,$a) {
-       
-    }
-    public function getPerimetro ($b,$a) {
-        
-    }
 
-    public function getDiagonale($a,$b){
-        
-    }
-     
+    $this->altezza = $altezza;
+    $this->base = $base;
+  }
 
+  public function getArea()
+  {
+    echo $this->mul($this->altezza, $this->base);
+  }
+  public function getPerimetro()
+  {
+    echo $this->sum($this->altezza, $this->base) + $this->sum($this->altezza, $this->base);
+  }
+
+  public function getDiagonale()
+  {
+    echo $this->sqr($this->sum($this->mul($this->altezza,$this->altezza),$this->mul($this->base, $this->base)));
+  }
 }
 
-// - Area => b*h
-// - Perimetro => 2b + 2h
-// - Diagonale =>  √ hˆ2 + bˆ2  (Tutto sotto radice).
 
-//$area = new Rettagolo(5,2);
-
-//$area->getArea(5,2);
-//$area->getPerimetro(5,2);
-//$area->getDiagonale(5,2);
+$rettangolo1= new Rettagolo (5,4);
+//$rettangolo1->getArea();
+//$rettangolo1->getPerimetro();
+//$rettangolo1->getDiagonale();
